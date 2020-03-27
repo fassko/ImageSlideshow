bb8d1fb5f58ab882963ddc48951a5ed4665a033c
Kristaps Grinbergs <fassko@gmail.com>
2020-03-27 21:31:01 +0200

    New versions

--- Package.swift
+++ Package.swift
@@ -22,9 +22,9 @@ let package = Package(
             targets: ["ImageSlideshowKingfisher"])
     ],
     dependencies: [
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.8.0"),
-        .package(url: "https://github.com/Alamofire/AlamofireImage.git", .branch("master")),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.13.2"),
+        .package(url: "https://github.com/Alamofire/AlamofireImage.git", .from("4.0.3")),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.6.1")
     ],
     targets: [
         .target(
